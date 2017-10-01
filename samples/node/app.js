62e3b6a4c6c3e857d02eae90633767601b9ec40f
Majid Khan <mkhan3189@gmail.com>
2017-10-01 18:08:42 +1100

    update

--- samples/node/app.js
+++ samples/node/app.js
@@ -11,27 +11,7 @@ var defaultLogger = residue.getLogger('default');
 
 var app = express();
 
-/*
-const residueParams = {
-    url: "localhost:8777",
-    access_codes: [
-        {
-            logger_id: "sample-app",
-            code: "a2dcb"
-        }
-    ],
-    application_id: "com.muflihun.securitybox",    
-    rsa_key_size: 2048,
-    utc_time: true,
-    time_offset: 0,
-    client_id: "muflihun00102030",
-    client_private_key: "/Users/majid.khan/Projects/residue/samples/clients/netcat/client-256-private.pem",
-    server_public_key: "/Users/majid.khan/Projects/residue/samples/clients/netcat/server-1024-public.pem"
-};
-*/
-
 app.get('*', function(req, res, next) {
-    //logger.info('Request: ' + req.url);
     return next();
 });
 
@@ -59,10 +39,9 @@ app.get('/', function (req, res) {
 app.listen(3009, function () {
     console.log('Open http://localhost:3009 on browser');
 
-    // Either you can use residueParams or loadConfiguration form file
-    // for this sample we use loadConfiguration
-	const confFile = 'client.conf.json';
+    const confFile = 'client.conf.json';
     if (residue.loadConfiguration(confFile)) {
-        residue.connect(/*residueParams*/);
+        residue.connect();
     }
+    // alternatively you could do residue.connect(config_json_object)
 });
