9e69a15150c0868a47b0c6bf153d76fbdd453a36
mkhan <mkhan@allhomes.com.au>
2018-03-27 10:00:44 +1100

    up

--- samples/via-native/index.js
+++ samples/via-native/index.js
@@ -1,13 +1,11 @@
 const residue = require('residue/lib/residue-native');
 
-const instance = new residue.Residue();
+console.log(`Library version: ${residue.version()}`);
 
-console.log(`Library version: ${instance.version()}`);
-
-instance.configure('{"url":"residue-server:8777","application_id":"com.muflihun.residue.sampleapp","rsa_key_size":2048,"utc_time":false,"time_offset":0,"dispatch_delay":1,"main_thread_id":"MainThread","client_id":"muflihun00102030","client_private_key":"keys/muflihun00102030.pem"}');
+residue.loadConfigurationFromJson('{"url":"residue-server:8777","application_id":"com.muflihun.residue.sampleapp","rsa_key_size":2048,"utc_time":false,"time_offset":0,"dispatch_delay":1,"main_thread_id":"MainThread","client_id":"muflihun00102030","client_private_key":"keys/muflihun00102030.pem"}');
 instance.connect();
 
-const logger = new residue.Logger("sample-app");
+const logger = residue.getLogger("sample-app");
 
 logger.info("blah");
 
