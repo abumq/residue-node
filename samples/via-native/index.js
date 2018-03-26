e1790cb261a023f5a0d7227576a6e77a04e66417
mkhan <mkhan@allhomes.com.au>
2018-03-27 10:29:42 +1100

    up

--- samples/via-native/index.js
+++ samples/via-native/index.js
@@ -1,20 +1,21 @@
 const residue = require('residue/lib/native');
+const logger = residue.getLogger('sample-app');
 
 console.log(`Library version: ${residue.version()}`);
 
-residue.loadConfiguration('{"url":"residue-server:8777","application_id":"com.muflihun.residue.sampleapp","rsa_key_size":2048,"utc_time":false,"time_offset":0,"dispatch_delay":1,"main_thread_id":"MainThread","client_id":"muflihun00102030","client_private_key":"keys/muflihun00102030.pem"}');
-instance.connect();
-
-const logger = residue.getLogger("sample-app");
-
-logger.info("blah");
-
-function a_fn()
-{
-    logger.debug('a debug log in func %s', 'with string fmt');
-    logger.verbose(5, 'a verbose log in func, verbose level: %d', 5);
+const confFile = 'client.conf.json';
+if (residue.loadConfiguration(confFile)) {
+    residue.connect();
 }
 
-logger.debug('a debug log %s', 'with string fmt');
+logger.info('simple log');
 
-a_fn();
+logger.info('array %s', [1, 2, 3]);
+
+var person = { 'name': 'Adam', 'age': 960, }
+logger.info('obj %s', person);
+
+logger.info('null %s', null);
+
+logger.info('undefined %s', undefined);
+
