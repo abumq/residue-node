3005164c89f86f21c12831fa9ebef21913dd9c9c
Majid <mkhan3189@gmail.com>
2018-03-31 20:19:53 +1100

    loggers file update

--- test/index.js
+++ test/index.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 console.log(`version: ${residue.version()} (${residue.type()})`);
 
-const getContents = () => fs.readFileSync('/tmp/logs/residue.log').toString('utf8');
+const getContents = () => fs.readFileSync('test/residue.log').toString('utf8');
 const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
 const testLog = async (fn, msg) => {
@@ -15,7 +15,7 @@ const testLog = async (fn, msg) => {
   return getContents().indexOf(msg) !== -1;
 }
 const detailedTest = async () => {
-    if (process.platform === 'linux' || process.platform === 'darwin---') {
+    if (process.platform === 'linux' || process.platform === 'darwin') {
       console.log('connecting...');
 
       residue.connect({
