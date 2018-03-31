92fac73b9d30b644b5f00f8bbb33382c866103a8
Majid <mkhan3189@gmail.com>
2018-03-31 20:17:21 +1100

    update test

--- test/index.js
+++ test/index.js
@@ -3,16 +3,68 @@ const fs = require('fs');
 
 console.log(`version: ${residue.version()} (${residue.type()})`);
 
+const getContents = () => fs.readFileSync('/tmp/logs/residue.log').toString('utf8');
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
-if (process.platform === 'linux' || process.platform === 'darwin--') {
-    console.log('connecting...');
+const testLog = async (fn, msg) => {
+  const logger = residue.getLogger('default');
 
-    residue.connect({
-      url: '127.0.0.1:8777',
-    });
-  
-    console.log('checking...');
-    console.log(fs.readFileSync('/tmp/logs/residue.log').toString('utf8'));
-    console.log('exiting');
-    process.exit(0);
+  fn(msg);
+  await sleep(500);
+
+  return getContents().indexOf(msg) !== -1;
 }
+const detailedTest = async () => {
+    if (process.platform === 'linux' || process.platform === 'darwin') {
+      console.log('connecting...');
+
+      residue.connect({
+        url: '127.0.0.1:8777',
+      });
+    
+      console.log('checking...');
+      
+      console.log(getContents());
+
+      console.log('Creating logger');
+      const logger = residue.getLogger('default');
+
+      console.log('Sending messages');
+
+      if (!await testLog(logger.info, 'info msg simple')) {
+        console.log('info failed');
+        process.exit(1);
+      }
+
+      if (!await testLog(logger.error, 'error msg simple')) {
+        console.log('error failed');
+        process.exit(1);
+      }
+
+      if (!await testLog(logger.debug, 'debug msg simple')) {
+        console.log('debug failed');
+        process.exit(1);
+      }
+
+      if (!await testLog(logger.trace, 'trace msg simple')) {
+        console.log('trace failed');
+        process.exit(1);
+      }
+
+      if (!await testLog(logger.warn, 'warn msg simple')) {
+        console.log('warn failed');
+        process.exit(1);
+      }
+
+      if (!await testLog(logger.fatal, 'fatal msg simple')) {
+        console.log('fatal failed');
+        process.exit(1);
+      }
+
+      console.log('exiting - all tests OK');
+      process.exit(0);
+  }
+}
+
+detailedTest();
+
