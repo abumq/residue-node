b1a4c51b6c3b73429d49804f08a45587c03d4d01
Majid <mkhan3189@gmail.com>
2018-03-31 16:03:49 +1100

    Update index.js

--- test/index.js
+++ test/index.js
@@ -1,6 +1,13 @@
 const residue = require('../');
-
+const fs = require('fs');
 
 console.log(`version: ${residue.version()} (${residue.type()})`);
 
 
+if (process.platform === 'linux' || process.platform === 'darwin') {
+    residue.connect({
+      url: '127.0.0.1:8777',
+    });
+  
+    console.log(fs.readFileSync('/tmp/logs/residue.log'));
+}
