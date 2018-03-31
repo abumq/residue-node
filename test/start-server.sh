ca715af11a2414421069d5e3fc024924413c35e1
Majid <mkhan3189@gmail.com>
2018-03-31 22:48:21 +1100

    update

--- test/start-server.sh
+++ test/start-server.sh
@@ -1,16 +1,26 @@
 TYPE=`uname | tr '[:upper:]' '[:lower:]'`
 
-# Download
-npm install -g residue-$TYPE
-
-#wget https://github.com/muflihun/residue/releases/download/v2.3.3/residue-2.3.3-$TYPE-debug-x86_64.tar.gz
-#tar -xf residue-2.3.3-$TYPE-debug-x86_64.tar.gz
-
-# Start
 export RESIDUE_HOME=`pwd`/test/server/
-#residue-2.3.3-$TYPE-debug-x86_64/residue --version
-#nohup residue-2.3.3-$TYPE-debug-x86_64/residue $RESIDUE_HOME/conf.json -v --force-without-root --residue-home=$RESIDUE_HOME &
-nohup residue-$TYPE $RESIDUE_HOME/conf.json -v --force-without-root --residue-home=$RESIDUE_HOME &
+
+USE_NPM="false"
+
+if [ "$USE_NPM" = "true" ];then
+    npm install -g residue-$TYPE
+    residue-$TYPE --version
+    nohup residue-$TYPE $RESIDUE_HOME/conf.json -v --force-without-root --residue-home=$RESIDUE_HOME &
+else
+
+    SERVER_VERSION=2.3.3
+
+    wget https://github.com/muflihun/residue/releases/download/v$SERVER_VERSION/residue-$SERVER_VERSION-$TYPE-debug-x86_64.tar.gz
+    tar -xf residue-$SERVER_VERSION-$TYPE-debug-x86_64.tar.gz
+
+    residue-$SERVER_VERSION-$TYPE-debug-x86_64/residue --version
+    nohup residue-$SERVER_VERSION-$TYPE-debug-x86_64/residue $RESIDUE_HOME/conf.json -v --force-without-root --residue-home=$RESIDUE_HOME &
+fi
+
 ls -l
+echo 'nohup.out contents: '
 cat nohup.out
+echo 'end of nohup.out contents'
 echo '' > test/residue.log
